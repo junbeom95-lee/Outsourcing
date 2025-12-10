63e19ca2d8a59cf4c3faf819e40e03d88648ae8a
LEE SEOJUN <junbeom95@gmail.com>
2025-12-10 18:02:45 +0900

    🔧 chore(activity): 활동 로그 API TODO 설계
    1. TODO : 전체 활동 로그 조회
    2. TODO : 내 활동 로그 조회

--- src/main/java/com/example/outsourcing/domain/activity/controller/ActivityController.java
+++ src/main/java/com/example/outsourcing/domain/activity/controller/ActivityController.java
@@ -10,5 +10,16 @@ public class ActivityController {
 
     private final ActivityService activityService;
 
+    //TODO 전체 활동 로그 조회 : getActivities()
+    //TODO URI : /api/activities, Method : GET
+    //TODO Param : page, size, type, taskId, startDate, endDate
+    //TODO Data : ActivityTotalResponse (id, type, userId, user, taskId, timestamp, description)
+    //TODO user : (id, username, name)
+
+    //TODO 내 활동 로그 조회 : getMyActivities()
+    //TODO URI : /api/activities/me, Method : GET
+    //TODO AuthenticationPrincipal Long userId
+    //TODO Data : ActivityMyResponse (id, userId, user, action, targetType, targetId, description, createdAt)
+    //TODO user : (id, username, name)
 
 }
--- src/main/java/com/example/outsourcing/domain/activity/service/ActivityService.java
+++ src/main/java/com/example/outsourcing/domain/activity/service/ActivityService.java
@@ -9,4 +9,13 @@ import org.springframework.stereotype.Service;
 public class ActivityService {
 
     private final ActivityRepository activityRepository;
+
+    //TODO 전체 활동 로그 조회
+    //TODO Param : ActivityQueryRequest (page, size, type, taskId, startDate, endDate)
+    //TODO Data : ActivityTotalResponse (id, type, userId, user, taskId, timestamp, description)
+
+
+    //TODO 내 활동 로그 조회
+    //TODO Param Long userId
+    //TODO Data : ActivityMyResponse (id, userId, user, action, targetType, targetId, description, createdAt)
 }
