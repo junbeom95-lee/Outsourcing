a63ce6b1c1d962f83c43c8e71fc304409a087e28
fantasystar94 <aniseed_3@naver.com>
2025-12-11 19:08:24 +0900

    ✨ feat: 대시보드 쿼리 호출 리팩토링 및 팀 호출 추가 구현
    - 잦은 jpql 호출을 방지하고 projection 으로 처리해 db 호출 빈도를 낮췄습니다.
    - user에 연관관계가 추가되었습니다.
    - 대시보드에 팀 관련 내용이 정상 작동됩니다.

--- src/main/java/com/example/outsourcing/domain/task/repository/TaskRepository.java
+++ src/main/java/com/example/outsourcing/domain/task/repository/TaskRepository.java
@@ -3,16 +3,22 @@ package com.example.outsourcing.domain.task.repository;
 import com.example.outsourcing.common.entity.Task;
 import com.example.outsourcing.common.entity.User;
 import com.example.outsourcing.common.enums.TaskStatus;
+import com.example.outsourcing.domain.task.dto.response.StatusCountDto;
+import com.example.outsourcing.domain.task.dto.response.TaskCountProjection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
+import org.yaml.snakeyaml.util.Tuple;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
@@ -38,32 +44,19 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
     @EntityGraph(attributePaths = "assignee")
     Optional<Task> findById(@Param("assigneeId") Long id);
 
-    @EntityGraph(attributePaths = "assignee")
-    @Query("select t from Task t where t.assignee.id = :assigneeId")
-    Optional<User> findByAssignee_Id(@Param("assigneeId")Long assigneeId);
-
-    @Query("select count(t) from Task t")
-    int countAll();
-
-    @Query("select count(t) from Task t where t.status = :status")
-    int countByTasks(@Param("status") TaskStatus status);
+    @Query("select t.status,count(t) from Task t group by t.status")
+    List<StatusCountDto> countByTasks();
 
     @Query("select count(t) from Task t where t.dueDate < :today")
-    int countByOverDueDate(@Param("today") LocalDateTime today);
+    Long countByOverDueDate(@Param("today") LocalDateTime today);
+
 
     @EntityGraph(attributePaths = "assignee")
-    @Query("select count(t) from Task t where t.assignee.id = :id")
-    double countByMyTasks(@Param("id")Long id);
-
-    @EntityGraph(attributePaths = "assignee")
-    @Query("select count(t) from Task t where t.assignee.id = :id and t.status = :status")
-    double countByMyTasksByStatusIdDone(@Param("id")Long id,@Param("status") TaskStatus status);
+    @Query("select t.status,count(t) from Task t where t.assignee.id = :id group by t.status")
+    List<TaskCountProjection> countByMyTasksGrouped(@Param("id")Long id);
 
     @Query("select t from Task t join fetch t.assignee a where a.id = :id and t.status = :status")
     List<Task> findTaskByMyStatus(@Param("id")Long id,@Param("status") TaskStatus status);
-
-    @Query("select DAYOFWEEK(createdAt) as day, count(*) as cnt from Task group by DAYOFWEEK(createdAt)")
-    List<Integer> countByCreatedByDay();
 
     @Query("select count(t) from Task t where DATE(t.dueDate) = :date")
     int countTasksDueDate(@Param("date")LocalDate date);
@@ -72,4 +65,5 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
     int countTasksDueDateByStatus(@Param("date")LocalDate date,TaskStatus status);
 
     List<Task> findAllByTitleContaining(String query);
+
 }
--- src/main/java/com/example/outsourcing/domain/team/repository/TeamRepository.java
+++ src/main/java/com/example/outsourcing/domain/team/repository/TeamRepository.java
@@ -1,9 +1,12 @@
 package com.example.outsourcing.domain.team.repository;
 
 import com.example.outsourcing.common.entity.Team;
+import com.example.outsourcing.common.enums.TaskStatus;
+import com.example.outsourcing.domain.task.dto.response.TeamTaskCountDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
 
 import java.util.List;
 import java.util.Optional;
@@ -30,4 +33,19 @@ public interface TeamRepository extends JpaRepository<Team, Long> {
     Optional<Team> findByIdWithUsers(@Param("teamId") Long teamId);
 
     List<Team> findAllByNameContaining(String query);
+
+        @Query("""
+        select task.status, count(task)
+        from Team t
+        join t.userTeamList ut
+        join ut.user u
+        join u.taskList task
+        where t.id = :teamId
+        group by task.status
+        """)
+        List<TeamTaskCountDto> countTeamTaskGroup(@Param("teamId") Long teamId);
+
+
+        @Query("select t from Team t join fetch t.userTeamList ut left join fetch ut.user u where u.id =:userId")
+        Optional<Team> findByTeamByUserId(@Param("userId")Long userId);
 }
