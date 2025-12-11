8f9ac6bfacb134eede1d5dc4e1f8a13ad3630c6e
LEE SEOJUN <junbeom95@gmail.com>
2025-12-11 23:55:17 +0900

    ♻ refactor(user): 유저 API 정규식 검증 보완
    1. username: 4~20자, 영문/숫자만 허용
    2. email: 유효한 이메일 형식
    3. password: 8자 이상, 영문/숫자/특수문자 조합
    4. name: 2~50자

--- src/main/java/com/example/outsourcing/domain/user/model/request/UserUpdateRequest.java
+++ src/main/java/com/example/outsourcing/domain/user/model/request/UserUpdateRequest.java
@@ -2,15 +2,21 @@ package com.example.outsourcing.domain.user.model.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UserUpdateRequest {
     @NotBlank
+    @Size(min = 2, max = 50, message = "이름을 2 ~ 50자로 입력하세요.")
     private String name;
     @NotBlank
-    @Email
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
     private String email;
     @NotBlank
+    @Size(min = 8, message = "비밀번호를 8자 이상으로 입력해주세요.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]).*$",
+            message = "비밀번호는 영문/숫자/특수문자로 입력하세요.")
     private String password;
 }
