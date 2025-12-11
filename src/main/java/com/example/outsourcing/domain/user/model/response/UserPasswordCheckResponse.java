6be0d254d92c73f65f4ce2a5ba9a7452be3c50b8
LEE SEOJUN <junbeom95@gmail.com>
2025-12-11 18:00:27 +0900

    ♻ refactor: DTO 어노테이션 변경
    1. 기존 : RequiredArgsConstructor
    2. 변경 : AllArgsConstructor

--- src/main/java/com/example/outsourcing/domain/user/model/response/UserPasswordCheckResponse.java
+++ src/main/java/com/example/outsourcing/domain/user/model/response/UserPasswordCheckResponse.java
@@ -1,11 +1,12 @@
 package com.example.outsourcing.domain.user.model.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class UserPasswordCheckResponse {
+
     private final boolean valid;
 
     public static UserPasswordCheckResponse from(boolean valid){
